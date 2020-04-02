df317ede4a1ec4d05e7d2383fcf9519480888f8e
Eugen Dzubenko <eugen.dzubenko@onix-systems.com>
2020-04-02 16:01:29 +0300

    fixed issues for touch devices
    # Conflicts:
    #	src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,17 @@
 <script lang="ts">
 import {Component, Vue} from 'vue-property-decorator';
 import MainLayout from '@/components/MainLayout.vue';
+import {store} from '@/store';
 
 @Component({
   components: {MainLayout},
 })
 export default class App extends Vue {
+  public created() {
+    document.addEventListener('touchstart', function touchhandler(e) {
+      store.commit('changeTouchDevice', true);
+      document.removeEventListener('touchstart', touchhandler, false);
+    }, false);
+  }
 }
 </script>
